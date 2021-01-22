b87210369e5380a4700b5a662b38f5900fac6835
William Mannard <wmannard@coveo.com>
2021-01-22 16:21:43 -0500

    completed Mosh course

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+const NavBar = ({ totalCounters }) => {
+  console.log('Navbar - Rendered');
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="./#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;

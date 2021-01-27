6100030aed84e44f8ce6de5115a20ee447a7d790
William Mannard <wmannard@coveo.com>
2021-01-27 17:46:10 -0500

    Search working

--- src/components/searchBox/searchBox.jsx
+++ src/components/searchBox/searchBox.jsx
@@ -2,6 +2,7 @@ import "./SearchBox.css";
 import React, { Component } from 'react';
 import { engine } from "../../engine";
 import { buildSearchBox } from "@coveo/headless";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 class SearchBox extends Component {
   state = {}
@@ -24,25 +25,21 @@ class SearchBox extends Component {
     return (
       <div className="card mt-5">
         <div className="card-body">
-          <h5 className="card-title">Standalone Search Box</h5>
+          <h5 className="card-title font-weight-bold">Standalone Search Box</h5>
           <input
             type="text"
             className="form-control m-1"
             value={this.state.value}
             id="StandaloneSearchBox"
-            onChange={(event) => {
-              console.log('Updated text');
-              this.headlessSearchBox.updateText(event.target.value);
-              this.setState({value: event.target.value});
-            }}
+            onChange={(e) => this.headlessSearchBox.updateText(e.target.value)}
+            onKeyUp={(e) => e.key === 'Enter' && this.headlessSearchBox.submit()}
           />
           <button className="btn btn-primary m-1" onClick={this.headlessSearchBox.submit}>Submit</button>
           <button className="btn btn-primary m-1" onClick={this.headlessSearchBox.clear}>Clear</button>
-          <h5>Suggestions</h5>
+          <br />
+          {this.state.suggestions.length > 0 && <span>Suggestions:</span>}
           {this.state.suggestions.map((suggestion) => (
-            <p>
-              {suggestion.rawValue}
-            </p>
+            <a><span className="badge badge-secondary m-1">{suggestion.rawValue}</span></a>
           ))}
         </div>
       </div>
